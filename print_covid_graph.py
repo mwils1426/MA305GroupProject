85fa27d75933928451f2b8a6afbca741637c14fd
ConnorCastleberry <97767153+ConnorCastleberry@users.noreply.github.com>
2022-04-26 15:33:44 -0500

    Updated 4/26 at 4:30

--- print_covid_graph.py
+++ print_covid_graph.py
@@ -11,12 +11,12 @@ Instrustions: Dates is x-values of data point, Numbers is the y-values of
 data points, Title should be a string. Title will default to "Percentage of
 population with COVID-19 per Month", if you input 'standard'.
 input formats:
-    Dates Format ex: [1/10/2021, 1/11/2021] list of dates, can't skip days!
+    Dates Format ex: [1/10/2021, 1/11/2021] list od strings, can't skip days!
     Numbers ex: [2, 6, 7, 8, 14, 23, 200] list of integers
 
 Example code:
     
-print_covid_graph([1/10/2021, 11/11/2021], [1, 10], 'standard')
+print_covid_graph(['1/10/2021', '11/11/2021'], [1, 10], 'standard')
 
 """
 import numpy as np
@@ -30,7 +30,7 @@ def print_covid_graph(Dates, Numbers, Title):
     
     # this section takes m/d/yyyy and turn it into into a number line
     for i in range(len(np.array(Dates ,dtype=object))):
-        x_datapoints = [x_datapoints, i]
+        x_datapoints.append(i)
     
     if first_date[-1] == last_date[-1]: #if start and end dates are in same year
         if first_date[1] != '/':  #if month in double digits
@@ -48,19 +48,45 @@ def print_covid_graph(Dates, Numbers, Title):
     else:
         dates_length = 'longer than year'
             
-            
-    fig = plt.figure()
-    plt.plot(x_datapoints, Numbers,'-')
-    plt.legend(['data'], loc='best')
-    #plt.ylim(1, 100)
-    #plt.ylim(Dates[0],Dates[-1]) # automatic y limits
-    #plt.xlim('January', 'December')  # month
+    #find max of numbers
+    max_number = np.max(Numbers)
+    # tick markers
+    num_ticks = np.ceil(len(Dates)/5)
+    a = 0
+    tick_mark = [Dates[0]]
+    for j in Dates:
+        a = a + 1
+        if a == num_ticks:
+            tick_mark.append(j)
+            a = 0
+    tick_mark.append(Dates[-1])
+    fig, ax= plt.subplots()
+    ax.plot(Dates, Numbers,'-')
+    ax.legend(['data'], loc='best')
+    plt.ylim(1, max_number*1.1)
+    #plt.xlim(x_datapoints[0],x_datapoints[-1]) # automatic y limits
+    plt.xlim(Dates[0], Dates[-1])  # month
     plt.xlabel('Month')
     plt.ylabel('Percentage of population with covid')
+    ax.set_xticks((tick_mark))
     if Title == 'standard':
-        plt.title('Percentage of population with COVID-19 per Month')
+        fig.suptitle('Percentage of population with COVID-19 per Month')
     else:
-        plt.title(Title)
-    plt.show()
-    
+        fig.suptitle(Title)
+    return plt.show()
+
+#==========================================================================
+
+#function display test code 
+
+test_numbers = np.array([12918813, 13436108, 15701638, 15647349, 16644019, 16436393,17409442, 1982448, 22697489, 25232343, 25199142, 26636596])
+test_dates = np.array(['1/1/2021', '2/1/2021', '3/1/2021', '4/1/2021', '5/1/2021', '6/1/2021', '7/1/2021', '8/1/2021', '9/1/2021', '10/1/2021', '11/1/2021', '12/1/2021'])
+title = 'Test plot'
+
+print_covid_graph(test_dates, test_numbers, title)
+
+
+
+
+
 
